6cc59a8fe5f4a8c8680f42a8633fd98e7e33c107
Ritesh Patil <riteshsp2000@gmail.com>
2021-06-26 16:33:11 +0530

    chore: Update Website config details
    - Update site metadata in config/website.js
    
    - Replace static assets with TEDxNITRourkela static assets

--- config/website.js
+++ config/website.js
@@ -1,28 +1,29 @@
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/blog your pathPrefix should be "blog"
-  siteTitle: 'My Blog', // Navigation and Site Title
-  siteTitleAlt: 'The blog of an egghead creator', // Alternative Site title for SEO
-  siteTitleShort: 'Starter Blog', // short_name for manifest
-  siteUrl: 'https://your-site.io', // Domain of your site. No trailing slash!
+  siteTitle: 'Blog | TEDxNITRourkela', // Navigation and Site Title
+  siteTitleAlt: 'The blog of TEDxNITRourkela', // Alternative Site title for SEO
+  siteTitleShort: 'TEDxNITRourkela Blog', // short_name for manifest
+  siteUrl: 'https://blog.tedxnitrourkela.com', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteLogo: 'images/logo.png', // Used for SEO and manifest, path to your image you placed in the 'static' folder
-  siteDescription: 'This is where they post things!',
-  author: 'Author', // Author for schemaORGJSONLD
-  organization: 'egghead.io LLC',
+  siteDescription:
+    'The official blog of TEDxNITRourkela, an independently organised TED Event by a group of passionate students of NIT Rourkela',
+  author: 'TEDxNITRourkela', // Author for schemaORGJSONLD
+  organization: 'TEDxNITRourkela',
 
-  // siteFBAppID: '123456789', // Facebook App ID - Optional
-  userTwitter: '@eggheadio', // Twitter Username
-  ogSiteName: 'egghead.io Gatsby Starter Blog', // Facebook Site Name
-  ogLanguage: 'en_US',
-  googleAnalyticsID: '',
+  siteFBAppID: '475685360131848', // Facebook App ID
+  userTwitter: '@tedxnitr', // Twitter Username
+  ogSiteName: 'TEDxNITRourkela', // Facebook Site Name
+  ogLanguage: 'en_UK',
+  googleAnalyticsID: 'UA-187973769-1',
 
   // Manifest and Progress color
-  themeColor: '#5348FF',
-  backgroundColor: '#2b2e3c',
+  themeColor: '#FF2B06',
+  backgroundColor: '#1a1a1a',
 
   // Social component
-  twitter: 'https://twitter.com/eggheadio/',
-  twitterHandle: '@eggheadio',
-  github: 'https://github.com/eggheadio/',
-  linkedin: '',
+  twitter: 'https://twitter.com/tedxnitr/',
+  twitterHandle: '@tedxnitr',
+  github: 'https://github.com/TEDxNITRourkela/',
+  linkedin: 'https://www.linkedin.cn/company/tedxnitrourkela',
 }
